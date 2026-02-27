f69f079f6cb03792126e1a22589f3340dfba7c7d
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-02-27 09:24:26 -0500

    mhc

--- test practice.py
+++ test practice.py
@@ -1,5 +1,8 @@
-def occupied (y, t):
-    occupied = (y,t)
-    print(occupied.count('c'))
-occupied ("c","...c")
-# count only works with single string :[
+# def occupied (y, t):
+#     occupied = (y,t)
+#     print(occupied.count('c'))
+# occupied ("c","...c")
+# # count only works with single string :[
+
+def occupied (y,t):
+    if (y[i]) == "C" and t 

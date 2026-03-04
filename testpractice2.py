bad7f9ed287fb3d4eae37362ea2e75cb316421d3
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-03-04 08:53:58 -0500

    Update testpractice2.py

--- testpractice2.py
+++ testpractice2.py
@@ -1,9 +1,10 @@
-
-text = "the red cat sat on the mat."
-texty = text.lower()
-amountt = text.count("t")
-amounts = text.count('s')
-if amountt > amounts:
-    print('English')
-else:
-    print('French')
+def countletter(text):
+    
+    texty = text.lower()
+    amountt = text.count("t")
+    amounts = text.count('s')
+    if amountt > amounts:
+        print('English')
+    else:
+        print('French')
+countletter("the red cat sat on the mat.")
